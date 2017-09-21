843a4ec3d42580620ddcdfcd82c80e5da804d2da
6e803675-ea32-4a7a-a025-21e1bd7d28ca <6e803675-ea32-4a7a-a025-21e1bd7d28ca@local>
2017-09-21 00:08:19 +0000

    add python

---
+++ pycalc.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+N = 1000
+X = np.random.random(N)
+Y = np.random.random(N)
+
+scores = []
+noscores = []
+for n in range(N):
+	x,y = X[n],Y[n]
+	if x**2 + y**2 < 1:
+		scores.append([x,y])
+	else:
+		noscores.append([x,y])
+	if n%10 == 0:
+		print(4*len(scores)/(len(scores)+len(noscores)))
